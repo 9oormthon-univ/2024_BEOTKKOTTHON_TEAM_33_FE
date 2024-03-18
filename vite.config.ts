2fb4dd2ada977520c556b5f1f092486eb9abeed9
seungsimdang <oak20005@naver.com>
2024-03-18 21:41:33 +0900

    Feat: svg 컴포넌트 형식으로 import 가능하게 변경

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -40,6 +41,7 @@ export default defineConfig({
         ]
       }
     }),
-    tsconfigPaths()
+    tsconfigPaths(),
+    svgr()
   ]
 });
