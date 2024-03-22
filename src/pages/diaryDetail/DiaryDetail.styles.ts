18f57f864b848f3742900379f939cded8302e7e0
seungsimdang <oak20005@naver.com>
2024-03-23 02:14:23 +0900

    Fix: ImagePagination 잘리는 현상 수정

--- src/pages/diaryDetail/DiaryDetail.styles.ts
+++ src/pages/diaryDetail/DiaryDetail.styles.ts
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const DiaryDetailWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   padding: 8px 16px 32px 16px;
 
