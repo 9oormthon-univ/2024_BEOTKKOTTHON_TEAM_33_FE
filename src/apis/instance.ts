86b30afa531eecd038f009e350ac15a63b18c602
222SeungHyun <93538221+seungsimdang@users.noreply.github.com>
2024-03-23 23:26:14 +0900

    HotFix: instance 설정

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -4,3 +4,8 @@ export const imgInstance: AxiosInstance = axios.create({
   baseURL: `https://picsum.photos/v2`,
   timeout: 5000
 });
+
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 5000
+});
