081f3554dbd1ff6d475587a3bfd0e261a1e8ae9c
JaeHyun137 <jhjh2002109@ewhain.net>
2024-03-24 05:42:18 +0900

    Feat : 로그인 추가

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,4 +1,8 @@
+import { useState, useEffect, memo } from "react";
+import { useNavigate } from "react-router-dom";
 import ImagePagination from "@components/ImagePagination/ImagePagination";
+import LoginBtn from "@assets/icons/kakao_login_large_wide.png";
+import axios from "axios";
 
 const images = [
   "../src/assets/icons/Voice Activated Device.jpg",
@@ -7,7 +11,64 @@ const images = [
 ];
 
 const Login = () => {
-  return <ImagePagination images={images} isLogin={true} />;
+  const [isClicked, setIsClicked] = useState(false);
+  const [idToken, setIdToken] = useState(false);
+  const navigate = useNavigate();
+  var params = new URL(document.URL).searchParams;
+  var code = params.get("code");
+  useEffect(() => {
+    if (code) {
+      axios({
+        method: "post",
+        url: "https://kauth.kakao.com/oauth/token",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data: {
+          code: code,
+          client_id: "e1b1aebac9030ea946e773bb00c46e30",
+          grant_type: "authorization_code",
+          redirect_uri: "http://localhost:5173/login"
+        }
+      })
+        .then((response) => {
+          console.log(response.data.id_token);
+          setIdToken(response.data.id_token);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [code]);
+  useEffect(() => {
+    if (idToken) {
+      axios
+        .post(import.meta.env.VITE_LOGIN_URL, {
+          idToken: idToken
+        })
+        .then((response) => {
+          sessionStorage.setItem("accessToken", response.data.data.accessToken);
+          sessionStorage.setItem("refreshToken", response.data.data.refreshToken);
+        });
+      navigate("/");
+    }
+  }, [idToken]);
+  if (isClicked) {
+    window.location.href =
+      "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e1b1aebac9030ea946e773bb00c46e30&redirect_uri=http://localhost:5173/login";
+  }
+  const kakaoHandleLogin = () => {
+    setIsClicked(true);
+  };
+
+  return (
+    <>
+      <ImagePagination images={images} isLogin={true} />;
+      <button onClick={() => kakaoHandleLogin()}>
+        <img alt="loginBtn" src={LoginBtn} style={{ marginTop: "1em" }} />
+      </button>
+    </>
+  );
 };
 
-export default Login;
+export default memo(Login);
