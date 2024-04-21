e0cd7ad262e76728daefe777f677a7621a24fdfc
SeungHyun Lee <93538221+seungsimdang@users.noreply.github.com>
2024-04-21 21:39:02 +0900

    HotFix: 빌드 에러 해결

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Global, ThemeProvider } from "@emotion/react";
 import globalStyles from "./styles/globalStyles";
 import theme from "themes/theme";
