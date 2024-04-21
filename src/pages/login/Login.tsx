429ca8700d5b363ecbff5f59acec5d687ed20046
SeungHyun Lee <93538221+seungsimdang@users.noreply.github.com>
2024-04-21 20:51:10 +0900

    HotFix: redirect URL 변경

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -30,7 +30,7 @@ const Login = () => {
           code: code,
           client_id: "e1b1aebac9030ea946e773bb00c46e30",
           grant_type: "authorization_code",
-          redirect_uri: "https://re-bom.shop/login"
+          redirect_uri: "https://www.re-bom.shop/login"
         }
       })
         .then((response) => {
