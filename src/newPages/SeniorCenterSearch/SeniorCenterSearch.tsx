773c0eaf01f570a39ca89120e269365b865c4078
SeungHyun Lee <93538221+seungsimdang@users.noreply.github.com>
2024-08-03 00:20:22 +0900

    Refactor: 검색 페이지 개선

--- src/newPages/SeniorCenterSearch/SeniorCenterSearch.tsx
+++ src/newPages/SeniorCenterSearch/SeniorCenterSearch.tsx
@@ -4,21 +4,51 @@ import SmileIcon from "@assets/icons/smileIcon.svg?react";
 import SadIcon from "@assets/icons/sadIcon.svg?react";
 import tmpData from "../tmpData.json";
 import ProductCard from "@components/ProductCard/ProductCard";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ProductType } from "@components/KakaoMap/KakaoMap.types";
+import { useNavigate } from "react-router-dom";
 
 const SeniorCenterSearch = () => {
   const defaultMsg = "지도를 옮겨서\n가까운 경로당을 찾아보세요";
   const noDataMsg = "검색 결과가 없어요.\n다시 한번 확인해주세요!";
+  const navigate = useNavigate();
+  const searchBarRef = useRef<HTMLInputElement | null>(null);
+  const searchBtnRef = useRef<HTMLButtonElement | null>(null);
 
   const [isSearched, setIsSearched] = useState(false);
+
+  useEffect(() => {
+    if (searchBarRef.current) searchBarRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    if (searchBarRef.current) {
+      searchBarRef.current.focus();
+
+      const handleKeyPress = (event: KeyboardEvent) => {
+        if (event.key === "Enter") {
+          searchBtnRef.current?.click();
+        }
+      };
+      searchBarRef.current.addEventListener("keypress", handleKeyPress);
+
+      return () => {
+        searchBarRef.current?.removeEventListener("keypress", handleKeyPress);
+      };
+    }
+  }, []);
 
   return (
     <>
       <S.SearchBarWrapper>
-        <BackArrow />
-        <S.SearchBar placeholder="검색어를 입력하세요" />
-        <S.SearchBtn onClick={() => setIsSearched(true)}>검색</S.SearchBtn>
+        <div onClick={() => navigate(-1)}>
+          <BackArrow />
+        </div>
+
+        <S.SearchBar placeholder="검색어를 입력하세요" ref={searchBarRef} />
+        <S.SearchBtn onClick={() => setIsSearched(true)} ref={searchBtnRef}>
+          검색
+        </S.SearchBtn>
       </S.SearchBarWrapper>
       <S.ListWrapper>
         {!isSearched ? (
